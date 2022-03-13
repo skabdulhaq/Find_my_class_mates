684bf6bf3846bcc54f091b3e597bd123e7d70f6b
sk.abdul haq <75989562+skabdulhaq@users.noreply.github.com>
2022-03-13 10:04:33 +0530

    requirements.txt added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, flash
 from templates.secrtes import *
 import pyrebase
+import gunicorn
 
 app = Flask(__name__)
 firebase_db = pyrebase.initialize_app(firebaseConfig)
